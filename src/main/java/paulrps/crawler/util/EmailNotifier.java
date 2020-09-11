e6ea28661acba754c130ba7e1b11f26d52096442
pricardo.silva <pricardo.silva@sefaz.pb.gov.br>
2020-09-11 20:07:02 -0300

    removed some logs changed application context to /api

--- src/main/java/paulrps/crawler/util/EmailNotifier.java
+++ src/main/java/paulrps/crawler/util/EmailNotifier.java
@@ -1,6 +1,5 @@
 package paulrps.crawler.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,7 +9,6 @@ import paulrps.crawler.domain.dto.EmailNotificationMessageDtoDto;
 import paulrps.crawler.domain.dto.NotificationMessageDto;
 import paulrps.crawler.domain.entity.User;
 
-@Slf4j
 @Component("EmailNotifier")
 public class EmailNotifier extends JobNotifier {
 
@@ -30,9 +28,6 @@ public class EmailNotifier extends JobNotifier {
     mail.setTo(user.getEmail());
     mail.setSubject(((EmailNotificationMessageDtoDto) message).getSubject());
     mail.setText(messageFormatter.formatBody(message.getBody()));
-    log.info(System.getenv("EMAIL_PASSWORD"));
-    log.info(System.getenv("EMAIL_USERNAME"));
-    log.info(System.getenv("DATABASE_URI"));
     javaMailSender.send(mail);
   }
 }
